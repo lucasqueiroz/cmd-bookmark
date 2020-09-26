da056f8e602e34444ad46c28ac4d3193443758f9
Lucas Queiroz <me@lucasqueiroz.dev>
2020-09-26 04:44:47 -0300

    Use CLI/UI to generate beautiful messages

--- lib/cli/installer.rb
+++ lib/cli/installer.rb
@@ -1,25 +1,25 @@
+require 'cli/ui'
+
 module CMDBookmark
   module CLI
     class Installer
+      DEFAULT_PATH = '/usr/local/bin'.freeze
+
       class << self
         def install
-          return if is_installed?
-    
-          path = '/usr/local/bin'
-          puts 'Installing CMD Bookmark...'
-          print "Please choose the installation folder (defaults to #{path}): "
-          tmp_path = gets.chomp || path
-          while not File.directory?(tmp_path) and not tmp_path.empty?
-            puts 'Invalid installation folder!'
-            print "Please choose the installation folder (defaults to #{path}): "
-            tmp_path = gets.strip
+          if is_installed?
+            puts ::CLI::UI.fmt('{{x}} CMD Bookmark is already installed!')
+            return
           end
-          path = tmp_path
-          puts 'CMD Bookmark was installed!'
+
+          path = ::CLI::UI.ask('Installation folder:', default: DEFAULT_PATH)
+          while not File.directory?(path)
+            puts ::CLI::UI.fmt('{{x}} Invalid installation folder!')
+            path = ::CLI::UI.ask('Installation folder:', default: DEFAULT_PATH)
+          end
+          puts ::CLI::UI.fmt('{{v}} CMD Bookmark was installed!')
         end
       
-        private
-  
         def is_installed?
           `which cmd-bookmark`
           $?.success?
--- lib/cmd-bookmark.rb
+++ lib/cmd-bookmark.rb
@@ -1,10 +1,9 @@
-$LOAD_PATH << '.'
-
+require 'cli/ui'
 require 'optparse'
-
 require_relative 'cli/installer'
 
 module CMDBookmark
+  ::CLI::UI::StdoutRouter.enable
   options = {}
 
   OptionParser.new do |opts|
@@ -14,4 +13,8 @@ module CMDBookmark
       CLI::Installer::install
     end
   end.parse!
+
+  unless CLI::Installer::is_installed?
+    abort(::CLI::UI.fmt('{{x}} You must install the CMD Bookmark utility first!'))
+  end
 end
